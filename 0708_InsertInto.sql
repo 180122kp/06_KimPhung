0fda841989f24dd014dc0f6ce4ffe87b7d2ed3a2
180122kp <nguyenthikimphung180122@gmail.com>
2023-08-07 20:28:41 +0700

    insert into

--- 0708_InsertInto.sql
+++ 0708_InsertInto.sql
@@ -18,11 +18,11 @@ ALTER TABLE Fresher_management ADD COLUMN VTI_Account VARCHAR(30) NOT NULL UNIQU
 INSERT INTO Fresher_management(TraineeID,Full_Name, Birth_Date,Gender,ET_IQ,ET_Gmath, ET_English,Training_Class,Evaluation_Note,VTI_Account)
 VALUES (1,'Nguyen Thi Kim Phung', DATE '2001-06-01','FEMALE',20,20,50,07080001,'Lop hoc rat tot','ID01'),
 (2,'Nguyen Thi Minh Phuoc', DATE '2001-12-22','FEMALE',20,20,50,07080001,'Lop hoc rat tot','ID02'),
-(3,'Nguyen Trinh Tra Giang', DATE '2001-01-18','FEMALE',20,20,50,07080001,'Lop hoc rat tot','ID03'),
-(4,'Van Phuc Huy', DATE '2001-06-01','FEMALE',20,20,50,07080001,'Lop hoc rat tot','ID04'),
-(5,'Nguyen Thi Kim', DATE '2001-06-01','FEMALE',20,20,50,07080001,'Lop hoc rat tot','ID05'),
-(6,'Nguyen Thi Kim A', DATE '2001-06-01','FEMALE',20,20,50,07080001,'Lop hoc rat tot','ID06'),
-(7,'Nguyen Thi Kim B', DATE '2001-06-01','FEMALE',20,20,50,07080001,'Lop hoc rat tot','ID07'),
-(8,'Nguyen Thi Kim C', DATE '2001-06-01','FEMALE',20,20,50,07080001,'Lop hoc rat tot','ID058'),
-(9,'Nguyen Thi Kim D', DATE '2001-06-01','FEMALE',20,20,50,07080001,'Lop hoc rat tot','ID09'),
-(10,'Nguyen Thi Kim E', DATE '2001-06-01','FEMALE',20,20,50,07080001,'Lop hoc rat tot','ID10');
+(3,'Nguyen Trinh Tra Giang', DATE '2001-01-18','FEMALE',20,20,50,07080002,'Lop hoc rat tot','ID03'),
+(4,'Van Phuc Huy', DATE '2001-06-01','FEMALE',20,20,50,07080002,'Lop hoc rat tot','ID04'),
+(5,'Nguyen Thi Kim', DATE '2001-06-01','FEMALE',20,20,50,07080003,'Lop hoc rat tot','ID05'),
+(6,'Nguyen Thi Kim A', DATE '2001-06-01','FEMALE',20,20,50,07080003,'Lop hoc rat tot','ID06'),
+(7,'Nguyen Thi Kim B', DATE '2001-06-01','FEMALE',20,20,50,07080003,'Lop hoc rat tot','ID07'),
+(8,'Nguyen Thi Kim C', DATE '2001-06-01','FEMALE',20,20,50,07080004,'Lop hoc rat tot','ID058'),
+(9,'Nguyen Thi Kim D', DATE '2001-06-01','FEMALE',20,20,50,07080004,'Lop hoc rat tot','ID09'),
+(10,'Nguyen Thi Kim E', DATE '2001-06-01','FEMALE',20,20,50,07080004,'Lop hoc rat tot','ID10');
